814219a0baa59d661e5b6b8dea1d085f11cfab85
carldotz <carldotz@163.com>
2014-11-24 23:01:56 +0800

    Framework of application.

--- scr/Quicksort.cc
+++ scr/Quicksort.cc
@@ -5,6 +5,41 @@
  *      Author: carl
  */
 
+#include <iostream>
+#include <stdlib.h>
 
+using std::cout;
+using std::endl;
+
+const size_t WIDTH = 100;
+
+void quicksort(int *a, size_t size) {
+
+}
+
+void gen_random(int *a, size_t size) {
+	for(size_t i=0;i<size;++i) {
+		*(a+i) = random()/10000000;
+	}
+}
+
+void display(int *a, size_t size) {
+	for(size_t i=0;i<size;++i) {
+		cout << *(a+i) << "\t";
+	}
+	cout << endl;
+}
+
+int main() {
+	int *data = NULL;
+	data = new int[WIDTH];
+	gen_random(data, WIDTH);
+	cout << "Before Sort:" << endl;
+	display(data, WIDTH);
+	quicksort(data, WIDTH);
+	cout << "After Sort:" << endl;
+	display(data, WIDTH);
+	delete [] data;
+}
 
 
