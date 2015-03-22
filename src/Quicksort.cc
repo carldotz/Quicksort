7db9f44da741b1859f9e9533cc69b7be5b52306f
carldotz <carldotz@163.com>
2015-03-23 01:34:22 +0800

    add quicksort2

--- src/Quicksort.cc
+++ src/Quicksort.cc
@@ -15,12 +15,38 @@ static int count = 0;
 
 const size_t WIDTH = 100;
 
-void quicksort(int *a, size_t size) {
+void quicksort2(int *a, size_t size) {
+	int key = a[size-1];
+	size_t i = 0, j= size - 1;
+	if(size > 1) {
+		while(i < j) {
+			while(i<j && a[i] <= key) {
+				++i;
+				++count;
+			}
+			if(i>=j) break;
+			else ++count;
+			a[j--] = a[i];
+			while(i<j && a[j] > key){
+				--j;
+				++count;
+			}
+			if(i>=j) break;
+			else ++count;
+			a[i++] = a[j];
+		}
+		a[i] = key;
+		quicksort2(a, i);
+		quicksort2(a+i+1, size-i-1);
+	}
+}
+
+void quicksort1(int *a, size_t size) {
 	int key = a[size - 1];
 	size_t i = 0, j= 0;
 	if(size > 1) {
 		for(i=0,j=0;j < size - 1;++j) {
-			if(a[j] < key) {
+			if(a[j] <= key) {
 				int t = a[i];
 				a[i] = a[j];
 				a[j] = t;
@@ -30,8 +56,8 @@ void quicksort(int *a, size_t size) {
 		}
 		a[size-1] = a[i];
 		a[i] = key;
-		quicksort(a, i);
-		quicksort(a+i+1, size-i-1);
+		quicksort1(a, i);
+		quicksort1(a+i+1, size-i-1);
 	}
 }
 
@@ -54,8 +80,16 @@ int main() {
 	gen_random(data, WIDTH);
 	cout << "Before Sort:" << endl;
 	display(data, WIDTH);
-	quicksort(data, WIDTH);
-	cout << "After Sort:" << count << endl;
+	count = 0;
+	quicksort1(data, WIDTH);
+	cout << "After Sort1:" << count << endl;
+	display(data, WIDTH);
+	gen_random(data, WIDTH);
+	cout << "Before Sort:" << endl;
+	display(data, WIDTH);
+	count = 0;
+	quicksort2(data, WIDTH);
+	cout << "After Sort2:" << count << endl;
 	display(data, WIDTH);
 	delete [] data;
 }
